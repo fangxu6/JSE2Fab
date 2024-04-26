8a896d26ae58c0b7d51d2d39b2f392c7673fe545
fangxu <fangxu6@gmail.com>
2024-04-26 08:00:02 +0800

    feat: 聚辰txt转tsk

---
+++ TXT转换TSK-聚辰/TSK-MERGE-SINF/Util/CMDTskToTxt.cs
@@ -0,0 +1,52 @@
+
+/*
+ * 作者：sky
+ * 时间：2008-06-25
+ * 作用：CMD 要求，将 tsk 格式转换成 txt 格式
+ */
+
+namespace DataToExcel
+{
+    using System;
+    using System.Collections;
+    using DataToExcel;
+
+    public class CMDTskToTxt : ConverterBase
+    {
+        public override void Convert(string tskfile, string txtfile)
+        {
+            // 读取来源文件
+            Dat source = new Dat(tskfile);  
+            source.Read();
+
+           
+
+            
+        }
+
+        public override IMappingFile Convert(IMappingFile source)
+        {
+            // 加载转换配置
+            ConvertConfig convertConfig = new ConvertConfig("tsk", "cmdtxt");
+
+            // 创建新文件
+            CmdTxt cmdtxt = new CmdTxt(source.FileName);
+
+            // 读取映射字段值
+            foreach (ConvertConfig.ConvertField f in convertConfig.Fields)
+            {
+                cmdtxt.Properties[f.To] = source.Properties[f.From];
+            }
+
+            // 导入 die 列表
+            cmdtxt.DieMatrix = source.DieMatrix.Clone();
+            cmdtxt.RowCount = cmdtxt.DieMatrix.YMax;
+            cmdtxt.ColCount = cmdtxt.DieMatrix.XMax;
+
+            // 旋转角度
+            cmdtxt.DeasilRotate(convertConfig.Rotate);
+
+            return cmdtxt;
+        }
+    }
+}
