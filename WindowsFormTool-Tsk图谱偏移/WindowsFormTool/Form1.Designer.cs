852931dcca28a7c8e7d59a1f6d6043f8538c6c70
fangxu <fangxu6@gmail.com>
2024-10-10 15:16:11 +0800

    feat: 图谱偏移Define类有修复

---
+++ WindowsFormTool-Tsk图谱偏移/WindowsFormTool/Form1.Designer.cs
@@ -0,0 +1,402 @@
+﻿namespace DataToExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.panel14 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel11 = new System.Windows.Forms.Panel();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.panel17 = new System.Windows.Forms.Panel();
+            this.panel18 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel14.SuspendLayout();
+            this.panel11.SuspendLayout();
+            this.panel17.SuspendLayout();
+            this.panel18.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel10.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(847, 107);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(208, 98);
+            this.panel2.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(208, 98);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.button3);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(3, 107);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(205, 98);
+            this.panel4.TabIndex = 22;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Enabled = false;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Location = new System.Drawing.Point(0, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(205, 98);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "X轴";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.button5);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel5.Location = new System.Drawing.Point(847, 3);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(208, 98);
+            this.panel5.TabIndex = 18;
+            // 
+            // button5
+            // 
+            this.button5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button5.Location = new System.Drawing.Point(0, 0);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(208, 98);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "浏览";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // panel7
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.panel7, 4);
+            this.panel7.Controls.Add(this.button2);
+            this.panel7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel7.Location = new System.Drawing.Point(3, 3);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(838, 98);
+            this.panel7.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Enabled = false;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(0, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(838, 98);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "TSK图谱文件路径";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.panel3, 5);
+            this.panel3.Controls.Add(this.progressBar1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(3, 498);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1052, 45);
+            this.panel3.TabIndex = 16;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressBar1.Location = new System.Drawing.Point(0, 0);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1052, 45);
+            this.progressBar1.Step = 17;
+            this.progressBar1.TabIndex = 0;
+            // 
+            // panel14
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.panel14, 2);
+            this.panel14.Controls.Add(this.button4);
+            this.panel14.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel14.Location = new System.Drawing.Point(3, 211);
+            this.panel14.Name = "panel14";
+            this.panel14.Size = new System.Drawing.Size(416, 281);
+            this.panel14.TabIndex = 12;
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button4.Enabled = false;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Location = new System.Drawing.Point(0, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(416, 281);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "说明：\r\nX轴移动，右移是正，左移是负；\r\nY轴移动，下移是正，上移是负。";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // panel11
+            // 
+            this.panel11.Controls.Add(this.comboBox2);
+            this.panel11.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel11.Location = new System.Drawing.Point(636, 107);
+            this.panel11.Name = "panel11";
+            this.panel11.Size = new System.Drawing.Size(205, 98);
+            this.panel11.TabIndex = 20;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "-1",
+            "-2",
+            "-3",
+            "-4",
+            "-5",
+            "-6",
+            "-7",
+            "-8",
+            "-9",
+            "-10"});
+            this.comboBox2.Location = new System.Drawing.Point(0, 0);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(205, 26);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // panel17
+            // 
+            this.panel17.Controls.Add(this.panel18);
+            this.panel17.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel17.Location = new System.Drawing.Point(214, 107);
+            this.panel17.Name = "panel17";
+            this.panel17.Size = new System.Drawing.Size(205, 98);
+            this.panel17.TabIndex = 21;
+            // 
+            // panel18
+            // 
+            this.panel18.Controls.Add(this.comboBox1);
+            this.panel18.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel18.Location = new System.Drawing.Point(0, 0);
+            this.panel18.Name = "panel18";
+            this.panel18.Size = new System.Drawing.Size(205, 98);
+            this.panel18.TabIndex = 22;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "-1",
+            "-2",
+            "-3",
+            "-4",
+            "-5",
+            "-6",
+            "-7",
+            "-8",
+            "-9",
+            "-10"});
+            this.comboBox1.Location = new System.Drawing.Point(0, 0);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(205, 26);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.panel17, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel11, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel10, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel14, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.panel3, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.panel7, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel5, 4, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 4, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 21.05263F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 21.05263F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 57.89474F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1058, 546);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // panel10
+            // 
+            this.panel10.Controls.Add(this.button7);
+            this.panel10.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel10.Location = new System.Drawing.Point(425, 107);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(205, 98);
+            this.panel10.TabIndex = 20;
+            // 
+            // button7
+            // 
+            this.button7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button7.Enabled = false;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button7.Location = new System.Drawing.Point(0, 0);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(205, 98);
+            this.button7.TabIndex = 2;
+            this.button7.Text = "Y轴";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.panel1, 3);
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(425, 211);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(630, 281);
+            this.panel1.TabIndex = 15;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(630, 281);
+            this.richTextBox1.TabIndex = 14;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1058, 546);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "TSK图谱基于XLSL恢复";
+            this.panel2.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel14.ResumeLayout(false);
+            this.panel11.ResumeLayout(false);
+            this.panel17.ResumeLayout(false);
+            this.panel18.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel10.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel17;
+        private System.Windows.Forms.Panel panel18;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel11;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Panel panel14;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ WindowsFormTool-Tsk图谱偏移/WindowsFormTool/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static DataToExcel.DieMatrix;
+
+namespace DataToExcel
+{
+    public partial class Form1 : Form
+    {
+        public string TSKFilePath;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog FileDialog = new OpenFileDialog())
+            {
+                FileDialog.Title = "选择 TSK 图谱文件";
+                FileDialog.RestoreDirectory = true; // 记住上次打开的目录
+
+                // 显示文件浏览对话框，并获取用户选择
+                DialogResult result = FileDialog.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    TSKFilePath = FileDialog.FileName;
+                    button2.Text = TSKFilePath;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                ToMapping();
+        }
+
+
+        private void ToMapping()
+        {
+            
+            if (string.IsNullOrWhiteSpace(TSKFilePath))
+            {
+                MessageBox.Show("请先选择 TSK初始图谱文件路径", "错误提醒", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            ///////-------------------------------TSK读取-------------------------//////
+            UpdateRichTextBox("开始解析初始TSK图谱\n");
+            Tsk tsk = LoadTsk(TSKFilePath);
+            UpdateRichTextBox("解析TSK图谱结束\n");
+
+            DieMatrix moveDieMatrix = tsk.DieMatrix.Clone();
+            //开始移动moveDieMatrix
+            UpdateRichTextBox("开始移动DieMatrix\n");
+            int moveX = Int32.Parse(comboBox1.Text);
+            int moveY = Int32.Parse(comboBox2.Text);
+
+            OffsetDir offsetDir = OffsetDir.X;
+            moveDieMatrix.Offset(offsetDir,moveX);
+            offsetDir = OffsetDir.Y;
+            moveDieMatrix.Offset(offsetDir, moveY);
+
+            tsk.DieMatrix = tsk.DieMatrix + moveDieMatrix;
+
+            UpdateRichTextBox("开始写入新TSK图谱\n");
+            string newTskFilePath = @"D:\New-Tsk\" + Path.GetFileName(TSKFilePath);
+            UpdateRichTextBox("生成图谱路径" + newTskFilePath + "\n");
+
+
+            this.progressBar1.Maximum = tsk.Rows * tsk.Cols;
+            this.progressBar1.Value = 0;
+
+
+
+            for (int k = 0; k < tsk.Rows * tsk.Cols; k++)
+            {
+                this.progressBar1.Value++;
+
+                
+            }
+
+            tsk.PassDie = 0;
+            tsk.FailDie = 0;
+            for (int k = 0; k < tsk.Rows * tsk.Cols; k++)
+            {
+                if (tsk.DieMatrix[k].Attribute == DieCategory.PassDie)
+                {
+                    tsk.PassDie++;
+                }
+                else if (tsk.DieMatrix[k].Attribute == DieCategory.FailDie)
+                {
+                    tsk.FailDie++;
+                }
+            }
+            tsk.TotalDie = tsk.PassDie + tsk.FailDie;
+
+
+            UpdateRichTextBox("开始生成新TSK图谱\n");
+            tsk.FullName = newTskFilePath;
+            tsk.Save();
+
+            if (MessageBox.Show("TSK新图谱生成，是否打开所在文件夹?", "confirm", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Process.Start(Path.GetDirectoryName(newTskFilePath));
+            }
+        }
+        private void Reverse(ref byte[] target)
+        {
+            int n1 = 0, n2 = target.Length - 1;
+            byte temp;
+            while (n1 < n2)
+            {
+                temp = target[n1];
+                target[n1] = target[n2];
+                target[n2] = temp;
+
+                n1++;
+                n2--;
+            }
+        }
+        private short ByteToInt16(ref byte[] target)
+        {
+            this.Reverse(ref target);
+            return BitConverter.ToInt16(target, 0);
+        }
+
+        private int ByteToInt32(ref byte[] target)
+        {
+            this.Reverse(ref target);
+            return BitConverter.ToInt32(target, 0);
+        }
+        //更新RichTextBox
+        private void UpdateRichTextBox(string message)
+        {
+            richTextBox1.Text += message;
+            Application.DoEvents();
+        }
+        
+        private Tsk LoadTsk(string tskFile)
+        {
+            Tsk tsk = new Tsk(tskFile);
+            tsk.Read();
+            //this.LotNo = tsk.LotNo.Trim();
+            return tsk;
+        }
+    }
+
+}
