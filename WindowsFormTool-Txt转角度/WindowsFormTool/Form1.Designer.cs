f336cf4e1f7c9dd51aa3b4090324d100bba7f0ef
fangxu <fangxu6@gmail.com>
2025-09-17 14:26:17 +0800

    feat: 追加txt转角度

---
+++ WindowsFormTool-Txt转角度/WindowsFormTool/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace WindowsFormTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel14 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel11 = new System.Windows.Forms.Panel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel14.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel11.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 22.07792F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.97403F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.97403F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.97403F));
+            this.tableLayoutPanel1.Controls.Add(this.panel6, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel4, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.panel14, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.panel7, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel11, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button5, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.panel5, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1410, 728);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // panel6
+            // 
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel6.Location = new System.Drawing.Point(1047, 4);
+            this.panel6.Margin = new System.Windows.Forms.Padding(4);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(359, 74);
+            this.panel6.TabIndex = 17;
+            // 
+            // panel4
+            // 
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(315, 4);
+            this.panel4.Margin = new System.Windows.Forms.Padding(4);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(358, 74);
+            this.panel4.TabIndex = 17;
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(681, 4);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(358, 74);
+            this.panel3.TabIndex = 16;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(1047, 168);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(358, 78);
+            this.panel2.TabIndex = 13;
+            // 
+            // panel14
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.panel14, 2);
+            this.panel14.Controls.Add(this.button4);
+            this.panel14.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel14.Location = new System.Drawing.Point(4, 254);
+            this.panel14.Margin = new System.Windows.Forms.Padding(4);
+            this.panel14.Name = "panel14";
+            this.panel14.Size = new System.Drawing.Size(669, 472);
+            this.panel14.TabIndex = 12;
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button4.Enabled = false;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Location = new System.Drawing.Point(0, 0);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(669, 472);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "说明：\r\nTXT图谱选择90度，文件路径D:\\New-TXT\\。\r\n只能有图谱文件，不能有多余其它信息。";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // panel7
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.panel7, 3);
+            this.panel7.Controls.Add(this.button2);
+            this.panel7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel7.Location = new System.Drawing.Point(4, 86);
+            this.panel7.Margin = new System.Windows.Forms.Padding(4);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(1035, 74);
+            this.panel7.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Enabled = false;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(0, 0);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(1035, 74);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "TXT图谱文件夹路径";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // panel11
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.panel11, 3);
+            this.panel11.Controls.Add(this.progressBar1);
+            this.panel11.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel11.Location = new System.Drawing.Point(4, 168);
+            this.panel11.Margin = new System.Windows.Forms.Padding(4);
+            this.panel11.Name = "panel11";
+            this.panel11.Size = new System.Drawing.Size(1035, 78);
+            this.panel11.TabIndex = 11;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressBar1.Location = new System.Drawing.Point(0, 0);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1035, 78);
+            this.progressBar1.Step = 17;
+            this.progressBar1.TabIndex = 0;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1047, 86);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(358, 74);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "选择文件夹并旋转";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // panel1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.panel1, 2);
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(681, 254);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(725, 472);
+            this.panel1.TabIndex = 15;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(725, 472);
+            this.richTextBox1.TabIndex = 14;
+            this.richTextBox1.Text = "";
+            // 
+            // panel5
+            // 
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel5.Location = new System.Drawing.Point(4, 4);
+            this.panel5.Margin = new System.Windows.Forms.Padding(4);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(303, 74);
+            this.panel5.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1410, 728);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "TSK顺时针转90度";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel14.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
+            this.panel11.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel14;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel11;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+    }
+}
+
---
+++ WindowsFormTool-Txt转角度/WindowsFormTool/Form1.cs
@@ -0,0 +1,189 @@
+﻿using DataToExcel;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+
+
+                string TskFile = dialog.SelectedPath;
+
+
+                DirectoryInfo TheFolder = new DirectoryInfo(TskFile);
+
+                foreach (FileInfo str in TheFolder.GetFiles("*", SearchOption.AllDirectories))
+                {
+                    ToRotating(str);
+                }
+            }
+
+           
+        }
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    ToRotating();
+        //}
+
+        private void ToRotating(FileInfo txtFile)
+        {
+            if (txtFile == null || txtFile.Length == 0)
+            {
+                MessageBox.Show("请先选择 TXT空图谱文件路径", "错误提醒", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            UpdateRichTextBox("开始旋转TXT图谱\n");
+
+            try
+            {
+                // 1. 配置文件路径（可根据实际情况修改）
+                string inputFilePath = txtFile.FullName; // 原始文件路径
+                string outputFilePath = @"D:\New-TXT\" + txtFile.Name; // 旋转后输出路径
+
+                // 2. 读取原始文件内容，过滤空行（避免旋转后出现无效空行）
+                List<string> originalLines = ReadNonEmptyLines(inputFilePath);
+                if (originalLines.Count == 0)
+                {
+                    UpdateRichTextBox("错误：原始文件无有效内容（或文件不存在）！");
+                    return;
+                }
+
+                // 3. 处理行长度一致性（以最长行为基准，短行补空格，确保旋转后格式整齐）
+                int maxLineLength = originalLines.Max(line => line.Length);
+                List<string> normalizedLines = NormalizeLineLengths(originalLines, maxLineLength);
+
+                // 4. 执行90度顺时针旋转
+                List<string> rotatedLines = Rotate90DegreesClockwise(normalizedLines, maxLineLength);
+
+                // 5. 保存旋转后的内容到新文件
+                SaveRotatedContent(rotatedLines, outputFilePath);
+
+                // 6. 提示操作成功
+                UpdateRichTextBox($"文件旋转完成！");
+                UpdateRichTextBox($"原始文件：{inputFilePath}");
+                UpdateRichTextBox($"输出文件：{outputFilePath}");
+            }
+            catch (Exception ex)
+            {
+                // 捕获并显示异常信息（如文件读写权限不足、路径错误等）
+                UpdateRichTextBox($"程序执行出错：{ex.Message}");
+            }
+
+        }
+
+
+        static void Rotete(string[] args)
+        {
+            
+        }
+
+        /// <summary>
+        /// 读取文件中所有非空行
+        /// </summary>
+        /// <param name="filePath">文件路径</param>
+        /// <returns>非空行列表</returns>
+        private static List<string> ReadNonEmptyLines(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException("指定的原始文件不存在", filePath);
+            }
+
+            // 读取所有行，并过滤掉空行（包括仅含空格的行）
+            return File.ReadAllLines(filePath)
+                       .Where(line => !string.IsNullOrWhiteSpace(line))
+                       .ToList();
+        }
+
+        /// <summary>
+        /// 标准化所有行的长度（短行末尾补空格，与最长行长度一致）
+        /// </summary>
+        /// <param name="lines">原始行列表</param>
+        /// <param name="maxLength">最长行长度</param>
+        /// <returns>长度统一的行列表</returns>
+        private static List<string> NormalizeLineLengths(List<string> lines, int maxLength)
+        {
+            List<string> normalized = new List<string>();
+            foreach (string line in lines)
+            {
+                // 若行长度不足，末尾补空格；若超出（理论上不会，因maxLength是最大值），则截断
+                string normalizedLine = line.PadRight(maxLength).Substring(0, maxLength);
+                normalized.Add(normalizedLine);
+            }
+            return normalized;
+        }
+
+        /// <summary>
+        /// 将文本内容按顺时针方向旋转90度
+        /// 原理：原始矩阵（行x列）转置后，反转每一行的顺序
+        /// </summary>
+        /// <param name="normalizedLines">长度统一的原始行列表</param>
+        /// <param name="maxLineLength">每行长度（列数）</param>
+        /// <returns>旋转后的行列表</returns>
+        private static List<string> Rotate90DegreesClockwise(List<string> normalizedLines, int maxLineLength)
+        {
+            List<string> rotated = new List<string>();
+            int rowCount = normalizedLines.Count; // 原始行数（旋转后变为列数）
+
+            // 遍历原始矩阵的列（旋转后变为行）
+            for (int col = 0; col < maxLineLength; col++)
+            {
+                char[] rotatedRow = new char[rowCount];
+                // 遍历原始矩阵的行（从最后一行到第一行，实现反转）
+                for (int row = 0; row < rowCount; row++)
+                {
+                    // 原始位置 (row, col) → 旋转后位置 (col, rowCount - 1 - row)
+                    rotatedRow[row] = normalizedLines[rowCount - 1 - row][col];
+                }
+                rotated.Add(new string(rotatedRow));
+            }
+
+            return rotated;
+        }
+
+        /// <summary>
+        /// 将旋转后的内容保存到文件
+        /// </summary>
+        /// <param name="rotatedLines">旋转后的行列表</param>
+        /// <param name="outputFilePath">输出文件路径</param>
+        private static void SaveRotatedContent(List<string> rotatedLines, string outputFilePath)
+        {
+            // 若输出目录不存在，自动创建
+            string outputDir = Path.GetDirectoryName(outputFilePath);
+            if (!Directory.Exists(outputDir))
+            {
+                Directory.CreateDirectory(outputDir);
+            }
+
+            // 写入文件（使用UTF-8编码，避免中文等特殊字符乱码）
+            File.WriteAllLines(outputFilePath, rotatedLines, System.Text.Encoding.UTF8);
+        }
+
+
+        //更新RichTextBox
+        private void UpdateRichTextBox(string message)
+        {
+            richTextBox1.Text += message;
+            Application.DoEvents();
+        }
+    }
+}
